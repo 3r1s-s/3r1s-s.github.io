6cd8b7cd608d5ddb7572dab95dabe4b3f905a296
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-12-30 18:33:14 -0700

    truly amazing

---
+++ blaze/script.js
@@ -0,0 +1,9 @@
+function laugh() {
+    var audio = new Audio('../src/assets/sounds/seinfeld-laugh-track.mp3');
+    audio.play();
+}
+
+function laugh2() {
+    var audio = new Audio('src/assets/sounds/seinfeld-laugh-track.mp3');
+    audio.play();
+}
